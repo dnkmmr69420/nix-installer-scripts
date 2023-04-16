1fa9065ac16b1e586e87fb3754927be816520ff9
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-16 12:14:30 -0500

    Update regular-nix-installer-selinux.sh

--- installer-scripts/regular-nix-installer-selinux.sh
+++ installer-scripts/regular-nix-installer-selinux.sh
@@ -51,5 +51,10 @@ bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scrip
 
 sleep 1
 
+sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
+sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installers/main/nix-app-icons.sh
+
+sleep 1
+
 echo "Reboot your system by typing"
 echo "systemctl reboot"
