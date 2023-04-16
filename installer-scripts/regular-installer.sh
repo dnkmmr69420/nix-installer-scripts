cbb78a6461ed53fa99cb3d4a245f1eb7ed9bd017
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-16 12:09:47 -0500

    Update regular-installer.sh

--- installer-scripts/regular-installer.sh
+++ installer-scripts/regular-installer.sh
@@ -5,3 +5,7 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "Creating a backup of nix"
 sleep 1
 bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installers/main/nix-backup.sh)
+
+sleep 1
+
+bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-linker.sh)
