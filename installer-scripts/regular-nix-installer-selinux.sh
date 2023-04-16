f619733fdea9ffe8b46a902d4337b084c31fa08d
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-16 14:27:37 -0500

    Update regular-nix-installer-selinux.sh

--- installer-scripts/regular-nix-installer-selinux.sh
+++ installer-scripts/regular-nix-installer-selinux.sh
@@ -43,7 +43,7 @@ sleep 1
 
 echo "Making a nix backup"
 
-bash <(https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup-selinux.sh)
+bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup-selinux.sh)
 
 sleep 1
 
