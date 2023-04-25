322716a3a3fc296e2d5243405f6d2a57ced5e7e1
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-25 11:03:26 -0500

    Update and rename inside-nix-distrobox.sh to nix-preexisting-container.sh

---
+++ nix-distrobox/nix-preexisting-container.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This is skipping the container creation process
+
+echo "what distrobox container would you like to use?"
+read container_name
+
+if type -p distrobox> /dev/null; then
+	echo "Installing nix..."
+	sleep 3
+	distrobox enter $container_name -- sh <(curl -L https://nixos.org/nix/install) --no-daemon
+	sleep 1
+	distrobox enter $container_name -- sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-distrobox/nix.conf
+	distrobox enter $container_name -- sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+	echo "Finished installing nix"
+	exit 0
+else
+	echo "Aborting installer. distrobox is not installed."
+	exit 1
+fi
