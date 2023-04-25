b37de0aa9da81c44b5eed1edebe6104a39801561
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-25 10:36:24 -0500

    Add files via upload

---
+++ nix-distrobox/nix-distrobox-setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This is meant to be ran outside of the distrobox container
+
+if type -p distrobox > /dev/null; then
+	echo "What would you like to name the distrobox?"
+	read distrobox_name
+	echo "Creating $distrobox_name ..."
+	distrobox create -i archlinux:latest -n $distrobox_name
+	echo "Installing nix in distrobox..."
+	distrobox enter $distrobox_name -- sh <(curl -L https://nixos.org/nix/install) --no-daemon
+	distrobox enter $distrobox_name -- sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-distrobox/nix.conf
+	distrobox enter $distrobox_name -- sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+	echo "Nix has been installed in the distrobox"
+	exit 0
+else
+	echo "Distrobox is not installed. Install distrobox first before running this."
+	exit 2
+fi
