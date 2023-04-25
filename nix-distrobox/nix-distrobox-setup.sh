e14e865d130af9ebc3887185cbf892e95945ff8a
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-25 10:15:01 -0500

    Update nix-distrobox-setup.sh

--- nix-distrobox/nix-distrobox-setup.sh
+++ nix-distrobox/nix-distrobox-setup.sh
@@ -3,6 +3,7 @@ echo "What would you like to name the distrobox?"
 read distrobox_name
 echo "Creating $distrobox_name"
 distrobox create -i archlinux:latest -n $distrobox_name
+echo "Installing nix in distrobox"
 distrobox enter $distrobox_name -- sh <(curl -L https://nixos.org/nix/install) --no-daemon
 distrobox enter $distrobox_name -- sudo rm /etc/nix/nix.conf
 distrobox enter $distrobox_name -- sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-distrobox/nix.conf
