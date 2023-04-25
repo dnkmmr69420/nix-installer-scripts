f8903534d0481eb4e93904dc56283c3dd368cdaa
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-04-25 11:52:02 -0500

    Update nix-preexisting-container.sh

--- nix-distrobox/nix-preexisting-container.sh
+++ nix-distrobox/nix-preexisting-container.sh
@@ -7,9 +7,9 @@ read container_name
 
 if type -p distrobox> /dev/null; then
 	echo "Installing nix..."
-	sleep 3
+	sleep 30
 	distrobox enter $container_name -- sh <(curl -L https://nixos.org/nix/install) --no-daemon
-	sleep 1
+	sleep 3
 	distrobox enter $container_name -- sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-distrobox/nix.conf
 	distrobox enter $container_name -- sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
 	echo "Finished installing nix"
