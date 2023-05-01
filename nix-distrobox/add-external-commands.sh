55d39c80e6bc0c71e38aeeaedccdef025c940aa0
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-01 11:50:05 -0500

    Create add-external-commands.sh

---
+++ nix-distrobox/add-external-commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+[ -f /run/host/usr/bin/flatpak ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/flatpak
+[ -f /run/host/usr/bin/rpm ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/rpm
+[ -f /run/host/usr/bin/dpkg ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/dpkg
+[ -f /run/host/usr/bin/pacman ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/pacman
+[ -f /run/host/usr/bin/xbps-querry ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/xbps-querry
+[ -f /run/host/usr/bin/emerge ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/emerge
+[ -f /run/host/usr/bin/eopkg ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/eopkg
