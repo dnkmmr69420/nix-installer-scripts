cce9eef040028fb9febc6287592780b13722d380
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-01 11:59:38 -0500

    Update add-external-commands.sh

--- nix-distrobox/add-external-commands.sh
+++ nix-distrobox/add-external-commands.sh
@@ -6,3 +6,7 @@
 [ -f /run/host/usr/bin/xbps-querry ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/xbps-querry
 [ -f /run/host/usr/bin/emerge ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/emerge
 [ -f /run/host/usr/bin/eopkg ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/eopkg
+[ -f /run/host/usr/bin/snap ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/snap
+sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/distrobox
+[ -f /run/host/usr/bin/podman ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/podman
+[ -f /run/host/usr/bin/docker ] && sudo ln -s /usr/bin/distrobox-host-exec /usr/bin/docker
