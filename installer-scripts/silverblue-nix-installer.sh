a1a030cdeede0106e45dd9ed6b100824d2baab5c
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-09 09:01:56 -0500

    Update silverblue-nix-installer.sh

--- installer-scripts/silverblue-nix-installer.sh
+++ installer-scripts/silverblue-nix-installer.sh
@@ -93,10 +93,6 @@ bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scrip
 
 sleep 1
 
-bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-linker.sh)
-
-sleep 1
-
 bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
 
 echo "Modifying configurations"
