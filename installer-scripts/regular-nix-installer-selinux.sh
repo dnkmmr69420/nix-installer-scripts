f62417b5ab0a129b03cf13a4222e1a0715dbc803
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-09 09:01:39 -0500

    Update regular-nix-installer-selinux.sh

--- installer-scripts/regular-nix-installer-selinux.sh
+++ installer-scripts/regular-nix-installer-selinux.sh
@@ -47,10 +47,6 @@ bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scrip
 
 sleep 1
 
-bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-linker.sh)
-
-sleep 1
-
 bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
 
 echo "Modifying configurations"
