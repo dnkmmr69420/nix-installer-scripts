f768fefc061be3d51cb17ad65336b040b0b585c1
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-10 10:42:32 -0500

    Update steamdeck-nix-installer.sh

--- installer-scripts/steamdeck-nix-installer.sh
+++ installer-scripts/steamdeck-nix-installer.sh
@@ -66,3 +66,32 @@ ExecStart=/usr/bin/systemctl restart --no-block nix-daemon.socket
 [Install]
 WantedBy=sysinit.target
 EOF
+
+echo "Enabling services..."
+
+sleep 3
+
+sudo systemctl enable --now ensure-symlinked-units-resolve.service
+
+echo "Preparing nix installer..."
+
+sleep 5
+
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+echo "Creating a backup of nix"
+sleep 1
+bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup.sh)
+
+sleep 1
+
+bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
+
+echo "Modifying configurations"
+
+sleep 1
+
+sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
+sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+
+echo "Finished installation"
