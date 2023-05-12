86ea9a9936db28d0adcdba4ddea3a885bfd6bf4d
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-12 08:48:33 -0500

    Update nix-void-linux-installer.sh

--- installer-scripts/nix-void-linux-installer.sh
+++ installer-scripts/nix-void-linux-installer.sh
@@ -4,6 +4,7 @@ echo "Preparing to install nix..."
 
 sleep 5
 
+sudo xbps-install -Sy wget
 sudo xbps-install -Sy nix
 
 echo "linking services"
@@ -28,3 +29,7 @@ sleep 1
 
 sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
 sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+
+sleep 1
+
+echo "Installation has finnished"
