0865d1cc556ec1be3be716147438741708518caf
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-19 12:01:08 -0500

    Update nix-compile-after-setup.sh

--- other-scripts/nix-compile-after-setup.sh
+++ other-scripts/nix-compile-after-setup.sh
@@ -5,7 +5,7 @@ sudo echo "Continuing..."
 
 PREFIX=$1
 
-echo "Linking nix"
+sudo echo "Linking nix..."
 sudo ln -s /opt/nixusr/bin/nix $PREFIX/bin
 sudo ln -s /opt/nixusr/bin/nix-shell $PREFIX/bin
 sudo ln -s /opt/nixusr/bin/nix-env $PREFIX/bin
@@ -19,8 +19,22 @@ sudo ln -s /opt/nixusr/bin/nix-instantiate $PREFIX/bin
 sudo ln -s /opt/nixusr/bin/nix-prefetch-url $PREFIX/bin
 sudo ln -s /opt/nixusr/bin/nix-store $PREFIX/bin
 
-echo "deleting and copying services..."
+sudo echo "linking share files..."
+sudo mkdir -p $PREFIX/share/bash-completion/completions
+sudo ln -s /opt/nixusr/share/bash-completion/completions/nix $PREFIX/share/bash-completion/completions/nix
+sudo mkdir -p $PREFIX/share/doc
+sudo ln -s /opt/nixusr/share/doc/nix $PREFIX/share/doc/nix
+sudo mkdir -p $PREFIX/share/fish/vendor_completions.d
+sudo ln -s /opt/nixusr/share/fish/vendor_completions.d/nix.fish $PREFIX/share/fish/vendor_completions.d/nix.fish
+sudo mkdir -p $PREFIX/share/zsh/site-functions
+sudo ln -s /opt/nixusr/share/zsh/site-functions/_nix $PREFIX/share/zsh/site-functions/_nix
+sudo ln -s /opt/nixusr/share/zsh/site-functions/run-help-nix $PREFIX/share/zsh/site-functions/run-help-nix
+
+sudo echo "deleting and copying services..."
 sudo rm -f /etc/systemd/system/nix-daemon.service
 sudo rm -f /etc/systemd/system/nix-daemon.socket
 sudo cp /opt/nixusr/lib/systemd/system/nix-daemon.service /etc/systemd/system/nix-daemon.service
 sudo cp /opt/nixusr/lib/systemd/system/nix-daemon.socket /etc/systemd/system/nix-daemon.socket
+
+sudo echo "linking configurations..."
+sudo ln -s /etc/nix /opt/nixusr/etc/nix
