25c7ff009db9bc6bfe9513418a2667ca5244ba54
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-19 10:01:21 -0500

    Update nix-compile.sh

--- other-scripts/nix-compile.sh
+++ other-scripts/nix-compile.sh
@@ -14,8 +14,10 @@ cd /opt/nix-git/nix
 nix develop
 nix develop .#native-clang11StdenvPackages
 
-nix-shell --run "./bootstrap.sh"
-nix-shell --run "./configure $configureFlags --prefix=$PREFIX_DIR"
-nix-shell --run "make -j $NIX_BUILD_CORES"
-nix-shell --run "make install"
-nix-shell --run "make installcheck -j $NIX_BUILD_CORES"
+sleep 1
+
+nix-shell --command "./bootstrap.sh"
+nix-shell --command "./configure $configureFlags --prefix=$PREFIX_DIR"
+nix-shell --command "make -j $NIX_BUILD_CORES"
+nix-shell --command "make install"
+nix-shell --command "make installcheck -j $NIX_BUILD_CORES"
