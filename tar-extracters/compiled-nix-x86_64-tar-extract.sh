7140ff1e14e6cb043193dfcdf5a0194381a9292b
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-20 11:02:18 -0500

    Create compiled-nix-x86_64-tar-extract.sh

---
+++ tar-extracters/compiled-nix-x86_64-tar-extract.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -f nixusr.tar.gz
+wget https://github.com/dnkmmr69420/nix-installer-scripts/releases/download/nix-x86_64-1.01/nixusr.tar.gz
+echo "enter password to continue extracting. This will overwite the current /opt/nixusr installation"
+sudo echo "Extracting nixusr.tar.gz"
+sudo rm -rf /opt/nixusr
+sudo tar -xf nixusr.tar.gz -C /opt
+sudo echo "If don't have selinux installed and you get errors about restorecon command not being found, ignore it."
+sudo restorecon -RF /opt/nixusr
