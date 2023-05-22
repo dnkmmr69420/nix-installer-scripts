02231d54aba3cde0e4be560aa47b7115a36b52a7
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-22 14:36:56 -0500

    Update nix-in-separate-profile.sh

--- other-scripts/nix-in-separate-profile.sh
+++ other-scripts/nix-in-separate-profile.sh
@@ -19,6 +19,31 @@ sudo ln -s /nix/var/nix/profiles/nix/bin/nix-instantiate $PREFIX/bin
 sudo ln -s /nix/var/nix/profiles/nix/bin/nix-prefetch-url $PREFIX/bin
 sudo ln -s /nix/var/nix/profiles/nix/bin/nix-store $PREFIX/bin
 
+sudo echo "linking share files..."
+sudo mkdir -p $PREFIX/share/bash-completion/completions
+sudo ln -s /nix/var/nix/profiles/nix/share/bash-completion/completions/nix $PREFIX/share/bash-completion/completions/nix
+sudo mkdir -p $PREFIX/share/doc
+sudo ln -s /nix/var/nix/profiles/nix/share/doc/nix $PREFIX/share/doc/nix
+sudo mkdir -p $PREFIX/share/fish/vendor_completions.d
+sudo ln -s /nix/var/nix/profiles/nix/share/fish/vendor_completions.d/nix.fish $PREFIX/share/fish/vendor_completions.d/nix.fish
+sudo mkdir -p $PREFIX/share/zsh/site-functions
+sudo ln -s /nix/var/nix/profiles/nix/share/zsh/site-functions/_nix $PREFIX/share/zsh/site-functions/_nix
+sudo ln -s /nix/var/nix/profiles/nix/share/zsh/site-functions/run-help-nix $PREFIX/share/zsh/site-functions/run-help-nix
+
+sudo echo "deleting and copying services..."
+sudo rm -f /etc/systemd/system/nix-daemon.service
+sudo rm -f /etc/systemd/system/nix-daemon.socket
+sudo cp /nix/var/nix/profiles/nix/lib/systemd/system/nix-daemon.service /etc/systemd/system/nix-daemon.service
+sudo cp /nix/var/nix/profiles/nix/lib/systemd/system/nix-daemon.socket /etc/systemd/system/nix-daemon.socket
+
+sudo echo "Configuring nix..."
+sudo tee /etc/profile.d/nix.sh <<EOF
+if [ -e '/nix/var/nix/profiles/nix/etc/profile.d/nix-daemon.sh' ]; then
+  . '/opt/nixusr/etc/profile.d/nix-daemon.sh'
+fi
+EOF
+
+
 
 sudo nix profile remove 0
 sudo nix profile remove 0
