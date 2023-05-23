59cf34e13f174fa0f557de3b6e495d67e33d2472
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-23 09:18:40 -0500

    Create configure.sh

---
+++ nix-out-of-default/build-scripts/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo echo "Configuring nix..."
+
+sudo tee /etc/profile.d/nix.sh <<EOF
+if [ -e '/opt/nixusr/etc/profile.d/nix-daemon.sh' ]; then
+  . '/opt/nixusr/etc/profile.d/nix-daemon.sh'
+fi
+EOF
+
+sudo tee /etc/sudoers.d/nix-ssl-cert-file <<EOF
+Defaults  env_keep += "NIX_SSL_CERT_FILE"
+EOF
+
+sudo mkdir /etc/systemd/system/nix-daemon.service.d
+
+sudo tee /etc/sudoers.d/nix-ssl-cert-file <<EOF
+Defaults  env_keep += "NIX_SSL_CERT_FILE"
+EOF
