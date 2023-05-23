c2c716f38c67464642c0d4d25a4abb3e95f5f979
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-23 09:27:28 -0500

    Update configure.sh

--- nix-out-of-default/build-scripts/configure.sh
+++ nix-out-of-default/build-scripts/configure.sh
@@ -14,6 +14,7 @@ EOF
 
 sudo mkdir -p /etc/systemd/system/nix-daemon.service.d
 
-sudo tee /etc/sudoers.d/nix-ssl-cert-file <<EOF
-Defaults  env_keep += "NIX_SSL_CERT_FILE"
+sudo tee /etc/systemd/system/nix-daemon.service.d/override.conf <<EOF
+[Service]
+Environment="NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
 EOF
