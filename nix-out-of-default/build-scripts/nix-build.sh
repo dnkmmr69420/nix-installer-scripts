49cd2c0e79a796dcd82ec9ed706e88f13f9379aa
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-23 08:13:13 -0500

    Update nix-build.sh

--- nix-out-of-default/build-scripts/nix-build.sh
+++ nix-out-of-default/build-scripts/nix-build.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-sudo mkdir -p /nix/nix
-sudo nix build nixpkgs#nix --out-link /nix/nix/nix
-sudo restorecon -RF /nix
+#!/usr/bin/sudo bash
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit 1
+fi
+
+mkdir -p /nix/nix
+nix build nixpkgs#nix --out-link /nix/nix/nix
+nix build nixpkgs#nix
+echo "if you don't have selinux and restorecon can't be found, ignore the error"
+restorecon -RF /nix
