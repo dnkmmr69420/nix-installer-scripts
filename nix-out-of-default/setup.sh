0e6abc3c9d6fca98eaa35b6f687980d4154b38d9
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-23 09:19:55 -0500

    Update setup.sh

--- nix-out-of-default/setup.sh
+++ nix-out-of-default/setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/sudo bash
 
+echo "enter sudo password to contine"
+sudo echo "Continuing..."
+
 curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/nix-build.sh | sudo bash
+sleep 1
 
 curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/nix-utils-install.sh | sudo bash
+sleep 1
 
 curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/nix-link.sh | sudo bash -s $1
+sleep 1
+
+curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/configure.sh | sudo bash
+sleep 1
