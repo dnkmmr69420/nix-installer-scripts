fc19cea3effaa9a4c060afd234b2e34e3c196e2d
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-23 09:59:13 -0500

    Update setup.sh

--- nix-out-of-default/setup.sh
+++ nix-out-of-default/setup.sh
@@ -12,6 +12,9 @@ sleep 1
 curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/nix-link.sh | sudo bash -s $1
 sleep 1
 
+curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/nix-service.sh | sudo bash
+sleep 1
+
 curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/build-scripts/configure.sh | sudo bash
 sleep 1
 
