fb909a662b851f5048d4fb26c534eca28b421d20
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-23 08:29:28 -0500

    Create nix-link.sh

---
+++ nix-out-of-default/build-scripts/nix-link.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+sudo echo "Linking services"
+sudo ln -s /nix/nix/nix/bin/nix $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-shell $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-env $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-build $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-channel $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-collect-garbage $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-copy-closure $PREFIX/bin
+sudo ln -s /nix/nix/nix/nix/bin/nix-daemon $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-hash $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-instantiate $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-prefetch-url $PREFIX/bin
+sudo ln -s /nix/nix/nix/bin/nix-store $PREFIX/bin
+
+sudo echo "linking share files..."
+sudo mkdir -p $PREFIX/share/bash-completion/completions
+sudo ln -s /nix/nix/nix/share/bash-completion/completions/nix $PREFIX/share/bash-completion/completions/nix
+sudo mkdir -p $PREFIX/share/doc
+sudo ln -s /nix/nix/nix/share/doc/nix $PREFIX/share/doc/nix
+sudo mkdir -p $PREFIX/share/fish/vendor_completions.d
+sudo ln -s /nix/nix/nix/share/fish/vendor_completions.d/nix.fish $PREFIX/share/fish/vendor_completions.d/nix.fish
+sudo mkdir -p $PREFIX/share/zsh/site-functions
+sudo ln -s /nix/nix/nix/share/zsh/site-functions/_nix $PREFIX/share/zsh/site-functions/_nix
+sudo ln -s /nix/nix/nix/share/zsh/site-functions/run-help-nix $PREFIX/share/zsh/site-functions/run-help-nix
+
+sudo echo "deleting and copying services..."
+sudo rm -f /etc/systemd/system/nix-daemon.service
+sudo rm -f /etc/systemd/system/nix-daemon.socket
+sudo cp /nix/nix/nix/lib/systemd/system/nix-daemon.service /etc/systemd/system/nix-daemon.service
+sudo cp /nix/nix/nix/lib/systemd/system/nix-daemon.socket /etc/systemd/system/nix-daemon.socket
+
+sudo echo "Enabling services..."
+sudo systemctl enable --now nix-daemon.socket
+sudo systemctl enable --now nix-daemon.service
