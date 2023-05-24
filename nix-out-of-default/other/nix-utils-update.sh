5f18a83d8d9d804b59ba3b5b5012073a7e834ce5
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-24 10:46:51 -0500

    Update nix-utils-update.sh

--- nix-out-of-default/other/nix-utils-update.sh
+++ nix-out-of-default/other/nix-utils-update.sh
@@ -5,11 +5,9 @@ PREFIX=$NIX_PREFIX_LOCATION
 wget -P /nix/nix/bin https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/bin/reset-nix
 wget -P /nix/nix/bin https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/bin/update-nix
 wget -P /nix/nix/bin https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/bin/update-nix-fallback
+wget -P /nix/nix/bin https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/bin/nix-utils
 
 chmod a+x /nix/nix/bin/reset-nix
 chmod a+x /nix/nix/bin/update-nix
 chmod a+x /nix/nix/bin/update-nix-fallback
-
-ln -s /nix/nix/bin/reset-nix $PREFIX/sbin
-ln -s /nix/nix/bin/update-nix $PREFIX/sbin
-ln -s /nix/nix/bin/update-nix-fallback $PREFIX/sbin
+chmod a+x /nix/nix/bin/nix-utils
