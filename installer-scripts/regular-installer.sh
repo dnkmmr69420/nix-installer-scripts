653dbe1a863343485001f99cc6eef823b3ccdfc7
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-25 10:55:40 -0500

    Update regular-installer.sh

--- installer-scripts/regular-installer.sh
+++ installer-scripts/regular-installer.sh
@@ -19,6 +19,7 @@ sleep 1
 
 sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
 sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh | bash
 
 echo "Building nix..."
 
