b10425c54af5a09e0bd3371606e52d27da7ad70e
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-25 08:53:00 -0500

    Update regular-installer.sh

--- installer-scripts/regular-installer.sh
+++ installer-scripts/regular-installer.sh
@@ -9,13 +9,9 @@ fi
 sudo echo "Preparing nix installation script"
 sleep 5
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
-echo "Creating a backup of nix"
+
+
 sleep 1
-bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup.sh)
-
-sleep 5
-
-bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
 
 echo "Modifying configurations"
 
@@ -23,5 +19,25 @@ sleep 1
 
 sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
 sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
+
+echo "Building nix..."
+
+sleep 1
+
+curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/setup.sh | bash -s /usr
+
+sudo echo "Cleaning up..."
+
+sleep 1
+
+sudo nix profile remove 0
+sudo nix profile remove 0
+
+sleep 1
+
+
+echo "Creating a backup of nix"
+sleep 1
+bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup.sh)
 
 echo "Finished installation"
