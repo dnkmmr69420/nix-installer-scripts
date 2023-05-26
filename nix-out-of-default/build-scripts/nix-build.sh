8765bd87782757d4586bfd1917afbd5022fc424f
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-26 08:06:41 -0500

    Update nix-build.sh

--- nix-out-of-default/build-scripts/nix-build.sh
+++ nix-out-of-default/build-scripts/nix-build.sh
@@ -8,5 +8,7 @@ fi
 mkdir -p /nix/nix
 nix build nixpkgs#nix --out-link /nix/nix/nix
 nix build nixpkgs#nix --out-link /nix/nix/fallback
+nix build nixpkgs#cacert --out-link /nix/nix/cacert
+nix build nixpkgs#cacert --out-link /nix/nix/cacart-fallback
 echo "if you don't have selinux and restorecon can't be found, ignore the error"
 restorecon -RF /nix
