0ca6b9a869d0096edbfa48c1ebfc781ced71cff3
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-30 13:44:20 -0500

    Update nix-void-linux-installer.sh

--- installer-scripts/nix-void-linux-installer.sh
+++ installer-scripts/nix-void-linux-installer.sh
@@ -15,6 +15,10 @@ sudo ln -s /etc/sv/nix-daemon /var/service/
 
 sudo source /etc/profile
 
+echo "Creating links..."
+sudo ln -s /nix/var/nix/profiles/default /nix/nix-profile
+sudo ln -s /nix/var/nix/profiles/default /var/nix-profile
+
 "setting up configs..."
 
 bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup.sh)
