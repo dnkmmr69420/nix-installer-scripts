3289a324ac7291423a5ce214443dbbfbc5d500d9
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-06-08 11:17:49 -0500

    Update regular-uninstaller.sh

--- uninstaller-scripts/regular-uninstaller.sh
+++ uninstaller-scripts/regular-uninstaller.sh
@@ -3,6 +3,7 @@ sudo echo "Uninstalling nix..."
 
 if [[ ! -d /etc/nix ]] ; then
   echo "Nix doesn't look like it has been installed so it has nothing to uninstall"
+if
 
 sudo systemctl stop nix-daemon.service
 sudo systemctl disable nix-daemon.socket nix-daemon.service
