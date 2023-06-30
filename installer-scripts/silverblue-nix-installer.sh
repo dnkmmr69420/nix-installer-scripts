89b761b48d4018cfae7053c92c9634e623e0bc73
rohanssrao <rohanssrao@gmail.com>
2023-06-30 12:57:40 -0400

    Update silverblue-nix-installer.sh
    untested

--- installer-scripts/silverblue-nix-installer.sh
+++ installer-scripts/silverblue-nix-installer.sh
@@ -1,22 +1,28 @@
-#!/bin/bash
-sudo sleep 2
-echo "Adding selinux content to /nix"
-sudo semanage fcontext -a -t etc_t '/nix/store/[^/]+/etc(/.*)?' ; sudo semanage fcontext -a -t lib_t '/nix/store/[^/]+/lib(/.*)?' ; sudo semanage fcontext -a -t systemd_unit_file_t '/nix/store/[^/]+/lib/systemd/system(/.*)?' ; sudo semanage fcontext -a -t man_t '/nix/store/[^/]+/man(/.*)?' ; sudo semanage fcontext -a -t bin_t '/nix/store/[^/]+/s?bin(/.*)?' ; sudo semanage fcontext -a -t usr_t '/nix/store/[^/]+/share(/.*)?' ; sudo semanage fcontext -a -t var_run_t '/nix/var/nix/daemon-socket(/.*)?' ; sudo semanage fcontext -a -t usr_t '/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
-sleep 1
+#!/usr/bin/env bash
+
 sudo mkdir /var/lib/nix
-sleep 1
-echo "adding selinux content to /var/lib/nix"
-sudo semanage fcontext -a -t etc_t '/var/lib/nix/store/[^/]+/etc(/.*)?' ; sudo semanage fcontext -a -t lib_t '/var/lib/nix/store/[^/]+/lib(/.*)?' ; sudo semanage fcontext -a -t systemd_unit_file_t '/var/lib/nix/store/[^/]+/lib/systemd/system(/.*)?' ; sudo semanage fcontext -a -t man_t '/var/lib/nix/store/[^/]+/man(/.*)?' ; sudo semanage fcontext -a -t bin_t '/var/lib/nix/store/[^/]+/s?bin(/.*)?' ; sudo semanage fcontext -a -t usr_t '/var/lib/nix/store/[^/]+/share(/.*)?' ; sudo semanage fcontext -a -t var_run_t '/var/lib/nix/var/nix/daemon-socket(/.*)?' ; sudo semanage fcontext -a -t usr_t '/var/lib/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
-echo "Creating service files"
-sleep 1
+
+echo "Setting SELinux context for /nix and /var/lib/nix"
+
+for path in "" "/var/lib"; do
+  sudo semanage fcontext -a -t etc_t $path'/nix/store/[^/]+/etc(/.*)?'
+  sudo semanage fcontext -a -t lib_t $path'/nix/store/[^/]+/lib(/.*)?'
+  sudo semanage fcontext -a -t systemd_unit_file_t $path'/nix/store/[^/]+/lib/systemd/system(/.*)?'
+  sudo semanage fcontext -a -t man_t $path'/nix/store/[^/]+/man(/.*)?'
+  sudo semanage fcontext -a -t bin_t $path'/nix/store/[^/]+/s?bin(/.*)?'
+  sudo semanage fcontext -a -t usr_t $path'/nix/store/[^/]+/share(/.*)?'
+  sudo semanage fcontext -a -t var_run_t $path'/nix/var/nix/daemon-socket(/.*)?'
+  sudo semanage fcontext -a -t usr_t $path'/nix/var/nix/profiles(/per-user/[^/]+)?/[^/]+'
+done
+
 echo "creating SSL cert file"
 
 sudo tee /etc/systemd/system/nix-daemon.service.d/override.conf <<EOF
 [Service]
 Environment="NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
 EOF
-sleep 1
-echo "Creating rootfs mkdir service"
+
+echo "Creating /nix mkdir service"
 
 sudo tee /etc/systemd/system/mkdir-rootfs@.service <<EOF
 [Unit]
@@ -32,8 +38,7 @@ ExecStart=mkdir -p '%f'
 ExecStopPost=chattr +i /
 EOF
 
-sleep 1
-echo "Creating nix.mount"
+echo "Creating nix.mount service"
 
 sudo tee /etc/systemd/system/nix.mount <<EOF
 [Unit]
@@ -51,79 +56,55 @@ Options=bind
 Type=none
 EOF
 
-sleep 1
 echo "Enabling mounting of /var/lib/nix to /nix and resetting SELinux context"
-sleep 1
 
-sudo systemctl daemon-reload ; sudo systemctl enable nix.mount ; sudo systemctl start nix.mount ; sudo restorecon -RF /nix
+sudo systemctl daemon-reload
+sudo systemctl enable nix.mount
+sudo systemctl start nix.mount
+sudo restorecon -RF /nix
 
-sleep 1
-
-echo "Temorarly setting SELinux to permissive"
+echo "Temporarily setting SELinux to permissive"
 
 sudo setenforce Permissive
 
-sleep 1
-
-echo "Preparing the nix install script..."
+echo "Running the Nix install script"
 
 sleep 5
 
 sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
-echo "Nix installer has Finished running..."
-sleep 1
-echo "Now copying service files..."
+echo "Nix installer has finished running"
 
-sleep 1
+echo "Copying service files"
 
-sudo rm -f /etc/systemd/system/nix-daemon.{service,socket} ; sudo cp /nix/var/nix/profiles/default/lib/systemd/system/nix-daemon.{service,socket} /etc/systemd/system/ ; sudo restorecon -RF /nix ; sudo systemctl daemon-reload ; sudo systemctl enable --now nix-daemon.socket
+sudo cp -f /nix/var/nix/profiles/default/lib/systemd/system/nix-daemon.{service,socket} /etc/systemd/system/
+sudo restorecon -RF /nix
+sudo systemctl daemon-reload
+sudo systemctl enable --now nix-daemon.socket
 
-sleep 1
-
-echo "Now setting SELinux back to Enforcing"
+echo "Setting SELinux back to enforcing"
 
 sudo setenforce Enforcing
 
-sleep 1
-
-
-
-sleep 1
-
 bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-scripts/nix-sudo-path.sh)
 
-echo "Modifying configurations..."
+echo "Modifying configurations"
 
-sleep 1
+sudo wget -O /etc/nix/nix.conf https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
+sudo wget -O /etc/profile.d/nix-app-icons.sh https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
 
-sudo rm -f /etc/nix/nix.conf ; sudo wget -P /etc/nix https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix.conf
-sudo rm -f /etc/profile.d/nix-app-icons.sh ; sudo wget -P /etc/profile.d https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/other-files/nix-app-icons.sh
-
-sudo echo "Building nix package manager..."
-
-sleep 1
+echo "Building Nix package manager"
 
 curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/nix-out-of-default/setup.sh | bash -s /usr/local
 
-sudo echo "Cleaning up..."
-
-sleep 1
-
-sudo nix profile remove 0
+echo "Cleaning up"
 sudo nix profile remove 0
 
-echo "Linking..."
-
-sleep 1
-
+echo "Linking"
 sudo ln -s /nix/var/nix/profiles/default /nix/nix-profile
 sudo ln -s /nix/var/nix/profiles/default /var/nix-profile
 
-sleep 1
-
-echo "Making a nix backup..."
-
+echo "Making a Nix backup"
 bash <(curl -s https://raw.githubusercontent.com/dnkmmr69420/nix-installer-scripts/main/backup-scripts/create-backup-selinux.sh)
 
 echo "Reboot your system by typing"
