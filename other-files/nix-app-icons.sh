d25fcdc967a80e8c1e602a6d63de593d58d83e40
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-07-03 11:02:48 -0500

    Update nix-app-icons.sh

--- other-files/nix-app-icons.sh
+++ other-files/nix-app-icons.sh
@@ -1,1 +1,2 @@
-XDG_DATA_DIRS="$HOME/.nix-profile/share:/nix/var/nix/profiles/default/share:$XDG_DATA_DIRS"
+export XDG_DATA_DIRS="$HOME/.nix-profile/share:/nix/var/nix/profiles/default/share:$XDG_DATA_DIRS"
+export KDEDIRS="$HOME/.nix-profile:/nix/var/nix/profiles/default:$KDEDIRS
